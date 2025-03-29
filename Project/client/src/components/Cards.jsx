a6b84e4fa250fec9f8818de479eb01728c6d6b31
Dimitar K Dimitrov <150704248+Dimitar101@users.noreply.github.com>
2025-03-29 15:01:08 +0200

    Update Cards.jsx use server fetch for cards

--- Project/client/src/components/Cards.jsx
+++ Project/client/src/components/Cards.jsx
@@ -1,16 +1,20 @@
 import { NavLink } from 'react-router';
-
+import { useEffect, useState } from "react";
 import '../app-css/card.css'
-import pic1 from '../assets/picture1.jpg'
 
-
-const cards = [
-    { img: pic1, name: 'Calculator', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', path: '/calculator' },
-    { img: pic1, name: 'Digital', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', path: '' },
-    { img: pic1, name: 'Digital 2', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', path: '' },
-];
 
 export default function Cards() {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3030/jsonstore/cards')
+            .then(res => res.json())
+            .then(data => setCards(Object.values(data)))
+            .catch(err => {
+                console.log(err.message);
+            });
+    }, []);
+    
 
     return (
         <>
@@ -19,12 +23,12 @@ export default function Cards() {
             <div className="cards">
                 {
                     cards.map((card) => (
-                        <article key={card.name}>
+                        <article key={card.gameName}>
                             <div className="card-picture">
                                 <img src={card.img} />
                             </div>
                             <div className="face-elements">
-                                <h3>{card.name}</h3>
+                                <h3>{card.gameName}</h3>
                                 <p>{card.description}</p>
                                 <NavLink className="card-button" to={card.path} >
                                     START
@@ -37,75 +41,3 @@ export default function Cards() {
         </>
     );
 }
-
-
-
-
-
-
-
-
-
-
-
-{/* const navigation = [
-    { name: 'Login', path: '/login' },
-    { name: 'Reg', path: '' },
-    { name: 'xxx', path: '' },
-];
-
-
-{
-    navigation.map((item) => (
-        <NavLink
-            key={item.name}
-            to={item.path}
-        // style={({ isActive }) => isActive ? { color: 'oklch(0.511 0.262 276.966)' } : {}}
-        >
-            {item.name}
-        </NavLink>
-    ))
-} */}
-
-
-
-// {/* <article>
-// <div className="card-picture">
-//     <img src={pic1} />
-// </div>
-// <div className="face-elements">
-//     <h3>Calculator</h3>
-//     <p>Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.</p>
-//     {/* <a href="#" className="button">START</a> */}
-//     <NavLink
-//         // key={1}
-//         to={'/calculator'}
-//         className="button"
-//     // style={({ isActive }) => isActive ? { color: 'oklch(0.511 0.262 276.966)' } : {}}
-//     >
-//         START
-//     </NavLink>
-// </div>
-// </article>
-
-// <article>
-// <div className="card-picture">
-//     <img src={pic1} />
-// </div>
-// <div className="face-elements">
-//     <h3>Digital Currency</h3>
-//     <p>Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.</p>
-//     <a href="#" className="button" onClick={startClickHandler}>START</a>
-// </div>
-// </article>
-
-// <article>
-// <div className="card-picture">
-//     <img src={pic1} />
-// </div>
-// <div className="face-elements">
-//     <h3>Digital Currency</h3>
-//     <p>Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.</p>
-//     <a href="#" className="button">START</a>
-// </div>
-// </article> */}
