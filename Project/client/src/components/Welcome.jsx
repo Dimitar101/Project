837a0c2b5d8d92a14ed8735f7bb7b4e938f68339
Dimitar K Dimitrov <150704248+Dimitar101@users.noreply.github.com>
2025-03-30 21:39:19 +0300

    Update Welcome.jsx make table with toggle; does not communicate with server

--- Project/client/src/components/Welcome.jsx
+++ Project/client/src/components/Welcome.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
+import WelcomeSubject from "./WelcomeSubject";
+
+
 export default function Welcome() {
+    const [subjectCheck, setSubjectCheck] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3030/jsonstore/subjects')
+            .then(res => res.json())
+            .then(data => {
+                const result = Object.values(data);
+                setSubjectCheck(result);
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    }, []);
+
+    const statusChangeHandler = (subjId) => {
+        setSubjectCheck(prev => prev.map(sub => sub._id === subjId ? { ...sub, isPractised: !sub.isPractised } : sub));
+    }
 
     return (
         <>
-            <h2>22222222222</h2>
+            <h1 className="welcome-heading">Have you practised today?</h1>
+            <hr />
+
+            <table>
+                <tbody>
+                    {subjectCheck.map(subj =>
+                        <WelcomeSubject
+                            key={subj._id}
+                            _id={subj._id}
+                            subject={subj.subject}
+                            isPractised={subj.isPractised}
+                            onStatusChange={statusChangeHandler}
+                        />
+                    )}
+                </tbody>
+            </table>
         </>
     );
 }
