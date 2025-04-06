a1a97a379107f209b830247ae6564188717aa7ac
Dimitar K Dimitrov <150704248+Dimitar101@users.noreply.github.com>
2025-04-06 16:54:46 +0300

    Update Multiplication.jsx add muliplication

--- Project/client/src/components/Multiplication.jsx
+++ Project/client/src/components/Multiplication.jsx
@@ -1,8 +1,50 @@
+import { useState } from 'react';
+
+
 export default function Multiplication() {
+    const [multiplicand, setMultiplicand] = useState('...');
+    const [multiplier, setMultiplier] = useState('...');
+    const [product, setProduct] = useState('');
+
+    const numBtnHandler = (e) => {
+        if (multiplicand == '...') {
+            setMultiplicand(e.target.textContent);
+        } else {
+            setMultiplier(e.target.textContent);
+            setProduct(+multiplicand * +e.target.textContent)
+        }
+    }
+
+    const clearAllHandler = () => {
+        setMultiplicand('...');
+        setMultiplier('...');
+        setProduct('');
+    }
+
+    const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
 
     return (
         <>
-            <div>UNDER CONSTERUCTION</div>
+            <div className='calculator-wrapper'>
+                <div className='multiplicand'>{multiplicand}</div>
+                <div className='multiplicationOperation'>*</div>
+                <div className='multiplier'>{multiplier}</div>
+
+                <div className='calculator'>
+                    <div className='btns'>
+                        <div className='digitBtns'>{digits.map(digit => <button onClick={numBtnHandler} key={digit}>{digit}</button>)}</div>
+                        <br />
+                        <div className='clearOperation'>
+                            <br />
+                            <button onClick={clearAllHandler}>Clear</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='product Result'>product:</div>
+                <div className='product Result'>{product}</div>
+            </div>
         </>
     );
 }
